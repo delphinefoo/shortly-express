57d1600b584680dd82535a84da9ad2ec2a8b4f7f
Delphine Foo-Matkin <delphinef@gmail.com>
2015-12-28 16:03:57 -0800

    Passed first three tests.

---
+++ app/config.js
@@ -0,0 +1,60 @@
+var Bookshelf = require('bookshelf');
+var path = require('path');
+
+var db = Bookshelf.initialize({
+  client: 'sqlite3',
+  connection: {
+    host: '127.0.0.1',
+    user: 'your_database_user',
+    password: 'password',
+    database: 'shortlydb',
+    charset: 'utf8',
+    filename: path.join(__dirname, '../db/shortly.sqlite')
+  }
+});
+
+db.knex.schema.hasTable('urls').then(function(exists) {
+  if (!exists) {
+    db.knex.schema.createTable('urls', function (link) {
+      link.increments('id').primary();
+      link.string('url', 255);
+      link.string('base_url', 255);
+      link.string('code', 100);
+      link.string('title', 255);
+      link.integer('visits');
+      link.timestamps();
+    }).then(function (table) {
+      console.log('Created Table', table);
+    });
+  }
+});
+
+db.knex.schema.hasTable('clicks').then(function(exists) {
+  if (!exists) {
+    db.knex.schema.createTable('clicks', function (click) {
+      click.increments('id').primary();
+      click.integer('link_id');
+      click.timestamps();
+    }).then(function (table) {
+      console.log('Created Table', table);
+    });
+  }
+});
+
+//create the users table if it doesn't exist
+db.knex.schema.hasTable('users').then(function(exists) {
+  if (!exists) {
+    //create an object representing our table
+    db.knex.schema.createTable('users', function(user) {
+      user.string('username', 255).primary();
+      user.string('password', 64);
+    });
+  }
+});
+
+/************************************************************/
+// Add additional schema definitions below
+/************************************************************/
+
+
+module.exports = db;
